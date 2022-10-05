8ba5a92af5d1df644e6c5bf1546786249ce57151
Samuellekpor <samuellekpor@gmail.com>
2022-10-05 21:13:59 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 import iconMove from './images/iconsMove.png';
 import iconEnter from './images/enter.png';
@@ -8,21 +7,21 @@ const tasks = [
   {
     description: 'Lorem ipsum dolor',
     completed: false,
-    index: 0
+    index: 0,
   },
   {
     description: 'Lorem ipsum dolor',
     completed: false,
-    index: 1
+    index: 1,
   },
   {
     description: 'lorem',
     completed: false,
-    index: 2
-  }
+    index: 2,
+  },
 ];
 
-function displayTasks(tasks) {
+const displayTasks = (tasks) => {
   const todoTitle = document.querySelector('.page-title');
   todoTitle.innerHTML = `<h1>Today's To Do</h1>
   <img class = "icon" src="${iconRefrech}" alt="refresh">`;
@@ -30,7 +29,7 @@ function displayTasks(tasks) {
   const inputField = document.querySelector('.input');
   inputField.innerHTML = `<input id="add-input" type="text" placeholder="Add your list...">
   <img class = "icon" src="${iconEnter}" alt="enter-key">`;
-  
+
   const todoContainer = document.querySelector('.todo-list');
   tasks.forEach((task) => {
     const taskContainer = document.createElement('li');
@@ -41,6 +40,6 @@ function displayTasks(tasks) {
 
     todoContainer.appendChild(taskContainer);
   });
-}
+};
 
 displayTasks(tasks);
