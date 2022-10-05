f7c55db062d6575f99cf65ad74951c58eb4ae8af
Samuellekpor <samuellekpor@gmail.com>
2022-10-05 20:38:50 +0000

    Create tasks array

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const tasks = [
+  {
+    description: 'Lorem ipsum dolor',
+    completed: false,
+    index: 0
+  },
+  {
+    description: 'Lorem ipsum dolor',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'lorem',
+    completed: false,
+    index: 2
+  }
+];
