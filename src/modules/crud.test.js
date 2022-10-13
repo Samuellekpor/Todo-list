0faac7f1311551179825b67af610539cca90bb96
Samuellekpor <samuellekpor@gmail.com>
2022-10-13 13:42:09 +0000

    Add update task test

--- src/modules/crud.test.js
+++ src/modules/crud.test.js
@@ -1,4 +1,4 @@
-import { addTask, removeTask } from '../../__mocks__/crud';
+import { addTask, removeTask, updateTask } from '../../__mocks__/crud';
 
 describe('add and remove tasks from array', () => {
   test('should add task to the list', () => {
@@ -30,4 +30,20 @@ describe('add and remove tasks from array', () => {
     expect(result).toEqual(expected);
     expect(result.length).toBe(expected.length);
   });
+
+  test('Should update a task', () =>  {
+    const arr = [
+      { index: 1, description: 'Taskone', completed: false },
+      { index: 2, description: 'Tasktwo', completed: false },
+    ];
+
+    const expected = [
+      { index: 1, description: 'Taskone', completed: false },
+      { index: 2, description: 'New task', completed: false },
+    ];
+    const result = updateTask(2,'New task',arr);
+    expect(result).toEqual(expected);
+
+  })
 });
+
